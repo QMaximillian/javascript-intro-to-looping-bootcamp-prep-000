d3bdd475841cd4a69fb42969f6b6f20a9dfae553
Quinn Lashinsky <quinnlashinsky@gmail.com>
2017-11-24 15:35:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,11 +23,10 @@ if (number === 0) {
 
 
 
-function doWhileLoop(array) {
+function doWhileLoop(array){
 return Math.random() >= 0.5
 do {
   array.pop();
 
 } while array.length > 0 && maybeTrue()
 return array
-}
